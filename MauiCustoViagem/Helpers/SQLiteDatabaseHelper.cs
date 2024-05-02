181b4eca6c616db93dfcd946dace01c753c1e44b
Usuario <Usuario@CPD03-M08>
2024-05-02 14:29:14 -0300

    Adicionar arquivos de projeto.

---
+++ MauiCustoViagem/Helpers/SQLiteDatabaseHelper.cs
@@ -0,0 +1,31 @@
+﻿using MauiCustoViagem.Models;
+using SQLite;
+
+namespace MauiCustoViagem.Helpers
+{
+    public class SQLiteDatabaseHelper
+    {
+        readonly SQLiteAsyncConnection _conn;
+
+        public SQLiteDatabaseHelper(string path)
+        {
+            _conn = new SQLiteAsyncConnection(path);
+            _conn.CreateTableAsync<Pedagio>().Wait();
+        }
+
+        public Task<int> Insert(Pedagio p)
+        {
+            return _conn.InsertAsync(p);
+        }
+
+        public Task<List<Pedagio>> GetAll()
+        {
+            return _conn.Table<Pedagio>().ToListAsync();
+        }
+
+        public Task<int> Delete(int id)
+        {
+            return _conn.Table<Pedagio>().DeleteAsync(i => i.Id == id);
+        }
+    }
+}
---
+++ MauiCustoViagem/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using MauiCustoViagem.Models;
+
+namespace MauiCustoViagem
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        public static Viagem via = new Viagem();
+
+        private async void btn_calcular_Clicked(object sender, EventArgs e)
+        {
+            via.Origem = txt_origem.Text;
+            via.Destino = txt_destino.Text;
+            via.Distancia = Convert.ToDouble(txt_distancia.Text);
+            via.Rendimento = Convert.ToDouble(txt_rendimento.Text);
+            via.Preco = Convert.ToDouble(txt_gas.Text);
+            
+            await Navigation.PushAsync(new Views.CalcularViagem());
+        }
+
+        private async void btn_AddPed_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Views.AddPedagio());
+        }
+
+        private async void btn_ListaPed_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Views.ListaPedagio());
+        }
+    }
+
+}
